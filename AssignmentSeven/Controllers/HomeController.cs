113c7543df8f847d6c29408ea28161f9344b8110
Kent Butler <KentDavidButler@gmail.com>
2019-02-21 21:18:09 -0500

    Initial commit

---
+++ AssignmentSeven/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using AssignmentSeven.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+
+//Everythign is in the about section!!!
+namespace AssignmentSeven.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.RedditTopTen = GitTopTen();
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public List<RedditPost> GitTopTen()
+        {
+            //Everythign is in the about section!!!
+            string URL = "https://www.reddit.com/r/aww/.json";
+
+            HttpWebRequest request = WebRequest.CreateHttp(URL);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader sr = new StreamReader(response.GetResponseStream());
+
+            string APIText = sr.ReadToEnd();
+            sr.Close();
+
+            JToken personData = JToken.Parse(APIText);
+
+            List<RedditPost> TopTen = new List<RedditPost>();
+            for (int i = 0; i < 11; i++)
+            {
+                string title = personData["data"]["children"][i+1]["data"]["title"].ToString();
+                string url = personData["data"]["children"][i + 1]["data"]["url"].ToString();
+                string thumb = personData["data"]["children"][i + 1]["data"]["thumbnail"].ToString();
+                TopTen.Add(new RedditPost(title, url, thumb));
+            }
+
+            return TopTen;
+        }
+    }
+}
---
+++ AssignmentSeven/Models/RedditPost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AssignmentSeven.Models
+{
+    public class RedditPost
+    {
+        public string Title { get; set; }
+        public string LinkURL { get; set; }
+        public string Thumbnail { get; set; }
+
+        public RedditPost(string title, string linkUrl, string thumbnail)
+        {
+            this.Title = title;
+            this.LinkURL = linkUrl;
+            this.Thumbnail = thumbnail;
+        }
+
+    }
+}
